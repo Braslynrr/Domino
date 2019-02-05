067ad779b5789c6433071cd37687da914973e683
Enrique Cabezas <enmeca99@gmail.com>
2019-02-04 19:18:56 -0600

    jeta

--- Domino/Archivo.cpp
+++ Domino/Archivo.cpp
@@ -4,6 +4,7 @@
 
 Archivo::Archivo()
 {
+	
 }
 
 
--- Domino/main.cpp
+++ Domino/main.cpp
@@ -1,11 +1,16 @@
 #include "caja.h"
+#include "Archivo.h"
 using std::cout;
 using std::endl;
 using std::cin;
 
 int main() {
+	/*
 	Caja C;
 	C.Revolver();
 	cout << &C;
+	*/
+	Archivo m;
+	
 	return 0;
 }
